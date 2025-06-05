9491ceaedf6448bc7dfee912c1829e0f3034cc81
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-05 00:35:51 -0300

    aula sobre comentarios finalizada

--- aula001.py
+++ aula001.py
@@ -1,5 +1,24 @@
 # Comentários em python são linhas de código que não são lidas pelo interpretador
 
 # Essa função exibe algo no terminal
-print('Exibindo mensagem na tela!')
-print(123)
+print('Exibindo mensagem na tela!') # podemos escrever comentários na mesma linha
+print(123) 
+# e abaixo da tela 
+# print(12345340) Podemos comentar um código
+
+""" Isso é uma DocString (podem ter multiplas linhas)
+
+Isso não 
+é
+um
+comentário 
+(o pograma ler isso mas não faz nada com elas)
+
+DocsStrings são para escrevermos notas e explicar os códigos
+
+"""
+
+'''
+Podemos usar docstrings para criar isso
+
+'''
