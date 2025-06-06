5b7e2b6c7c73276625f0ac51e5b80bfde990fc74
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-06 11:55:06 -0300

    aprendi sobre o tipo boolean

---
+++ aula005.py
@@ -0,0 +1,11 @@
+# tipo boolean (bool) - True ou False (Sim ou Não)
+
+print(5 > 4) # 5 é maior que 4? True
+print(5 == 5) # 5 é igual a 5 ? True
+print(4 > 5) # 4 é maior que 5? False
+
+# == é igual em programação e = é atribuição
+# > maior que
+
+# Verificando o tipo de dado dessas comparações 
+print(type(10 == 10)) # ele vai exibir o tipo de dado do resultado da comparação, nesse caso o tipo é bool
