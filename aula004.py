c64809d42dc776ed0f4477f547cf96c18fbfd36a
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-06 11:49:24 -0300

    aprendi sobre numeros int e float

---
+++ aula004.py
@@ -0,0 +1,15 @@
+# int e float
+
+# int são numeros inteiros 
+print(1, 2, 3, -3, -4, 0) # exemplos de números inteiros
+
+# float são numeros com pontos decimais 
+print(1.2, 3.4, 0.1, -1.23) # exemplos de números decimais 
+
+# Descobri o tipo de cada valor 
+print(type(1)) # int 
+print(type(1.2)) # float
+print(type('nelson')) # string
+
+# type é uma classe e não função - porém, todos os nomes que começam com letra minuscula e possuem () no final são funções (por convenção)
+# tudo é python é um objeto, por isso os resultados são 'class 'int''
