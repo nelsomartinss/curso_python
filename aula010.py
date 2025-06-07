5fc5d2185f59d950a41446a6d78c6f486db98933
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-07 17:54:15 -0300

    aprendi sobre concatenacao e repeticao de strings

---
+++ aula010.py
@@ -0,0 +1,11 @@
+# Concatenação e repetição
+
+# O operador de adição também serve para concatenar se estiver usando ele com duas strings
+concatencao = 'Nelson' + ' ' + 'Martins' # o resultado será Nelson Martins 
+
+# O operador de multiplicação irá fazer a repetição se ele for usado em conjunto com uma string
+
+repetir_tres_vezes = 'nelson' * 3 # ele ira repetir a string 3 vezes
+
+print(concatencao)
+print(repetir_tres_vezes)
