5f8aa1b5575f913a28c1371ee9a296833896f8b0
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-07 18:00:09 -0300

    aprendi sobre concatenacao  e repeticao

--- aula010.py
+++ aula010.py
@@ -4,7 +4,6 @@
 concatencao = 'Nelson' + ' ' + 'Martins' # o resultado será Nelson Martins 
 
 # O operador de multiplicação irá fazer a repetição se ele for usado em conjunto com uma string
-
 repetir_tres_vezes = 'nelson' * 3 # ele ira repetir a string 3 vezes
 
 print(concatencao)
