5b9edd868379eefa0b82ddc7ebefebea52b9fe01
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-07 13:13:06 -0300

    aprendi sobre variaveis

---
+++ aula007.py
@@ -0,0 +1,18 @@
+# Variáveis - espaços na memória para guardar dados
+
+# Variáveis precisam de um nome e esse nome não poder começar com letras maiusculas (por convensão), numeros, underlines e simbolos
+
+# Inicie o nome com letras minusculas, separe as palavras com underline (sneake case)
+
+# Sempre utilize nomes claros para as variaveis, o nome precisa refletir exatamente o que esta na variável
+
+# exemplos:
+primeiro_nome = 'Nelson' # string
+segundo_nome = 'Martins' # string
+idade = 24 # int
+altura = 1.74 # float 
+peso = 65.7 # float
+maior_de_idade = idade >= 18 # retornando um valor booleano
+
+# exibindo
+print(f'{primeiro_nome} {segundo_nome} tem {idade} anos de idade, pesa {peso}kg e possui {altura} de altura')
