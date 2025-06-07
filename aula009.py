ecde5a0d9cd68ca6551f8c4be473824f8fb2f075
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-07 13:35:08 -0300

    aprendi sobre operadores aritmeticos

---
+++ aula009.py
@@ -0,0 +1,29 @@
+# Operadores aritmeticos 
+
+# Adição 
+adicao = 10 + 12
+print(adicao)
+
+# subtração
+subtracao = 5 - 2
+print(subtracao)
+
+# multiplicação
+multiplicacao = 10 * 6
+print(multiplicacao)
+
+# divisão normal/decimal - sempre retornará um número decimal
+divisao_decimal = 10 / 2 # Mesmo o valor sendo inteiro ele retorna 5.0
+print(divisao_decimal)
+
+# divisao inteira - sempre retorna o valor inteiro
+divisao_inteira = 10 // 3 
+print(divisao_inteira)
+
+# modulo/resto da divisão - sempre retorna o que restou da divisao
+resto = 10 % 3 # 10 dividido por 3 da 9 e resta 1, esse é o resultado
+print(resto)
+
+# exponencial - Elevar numeros 
+exponencial = 2 ** 2 # 2x2 = 4
+print(exponencial)
