4126e414f704d2cab3beaaada0293eb434ce259e
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-07 13:24:07 -0300

    primeiro exercicio resolvido

---
+++ aula008.py
@@ -0,0 +1,15 @@
+# Exercicio
+
+nome = 'Nelson'
+sobrenome = 'Martins'
+idade = 24
+ano_nascimento = 2025 - idade
+maior_de_idade = idade >= 18 
+altura_metros = 1.74
+
+print(f'Nome: {nome}')
+print(f'Sobrenome: {sobrenome}')
+print(f'Idade: {idade}')
+print(f'Ano de Nascimento: {ano_nascimento}')
+print(f'É maior de idade? {maior_de_idade}')
+print(f'Altura: {altura_metros}')
