78a7548f7d556a675ef99301e4ead60d7e27a52d
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-08 15:02:20 -0300

    exercicio sobre imc finalizado

---
+++ aula012.py
@@ -0,0 +1,13 @@
+# Exercicio - Calculando o IMC
+
+# Entradas
+nome = 'Nelson Martins'
+altura = 1.74
+peso = 65.65
+
+# Processamento
+imc = peso / altura ** 2
+
+# Saída
+print(f'{nome} tem {altura} de altura, pesa {peso}Kg e seu IMC é de {imc:.2f}')
+print(...) # Ellipsis - usado para muitas coisas, não gera erro no código
