42ae5b7327ae3f9e993d0c378d682a28242dbce0
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-08 14:55:08 -0300

    aprendi sobre a ordem de prescedencia dos operadores

---
+++ aula011.py
@@ -0,0 +1,11 @@
+# Precedencia de operadores
+
+''' Ordem dos operadores
+    1. ()
+    2. ** 
+    3. * / // %
+    4. + -
+'''
+#                    1         3       1           2
+calculo = (32.5 / 2 + 20 / 2) + (80 + 118) + 98.91 / 2 
+print(calculo)
