2ee983f7b4c5b6c95ac5d437bbaa0b9ad2c8de66
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-09 18:00:54 -0300

    aprendi os operadores de comparacao

---
+++ aula019.py
@@ -0,0 +1,24 @@
+# Operadores de comparação
+'''
+> maior
+>= maior ou igual
+< menor
+<= menor ou igual 
+== igual
+!= diferente
+'''
+# sempre que usarmos esses opradores o resultado será booleano
+
+maior = 2 > 1 # 2 é maior do que 1 ?  True
+maior_ou_igual = 1 >= 1 # 1 é maior ou igual a 1 ? True
+menor = 1 < 2 # 1 é menor do que 2 ? True
+menor_ou_igual = 2 <= 2 # 2 é menor ou igual a 2 ? True
+igual = 1 == 1 # 1 é igual a 1 ? True
+diferente = 'nelson' != 'maria' #'nelson' é igual a 'maria' ? True
+
+print(maior)
+print(maior_ou_igual)
+print(menor)
+print(menor_ou_igual)
+print(igual)
+print(diferente)
