079d7fb413091573eeed28b862a6eed0cb2a3b35
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-09 11:44:46 -0300

    aprendi sobre if, elif, else e identação

---
+++ aula017.py
@@ -0,0 +1,14 @@
+# if e else - identação e fluxo do interpretador
+
+condicao = True 
+
+if condicao: # se 'condição' - assumindo que é True, existir
+    print(True) # os blocos dentro do if e else precisam ter 4 espaços de identação
+else: # precisam ter sempre os :
+    print(False)
+
+print('isso tá fora do else') # o código não identado não está dentro da condição
+
+'''
+Só podemos ter um if, o resto tem que ser 'elif', e só podemos ter um else
+'''
