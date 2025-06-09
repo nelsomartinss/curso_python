b5f100e5fbce43cbd934ef647addc876bf66c8de
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-09 11:40:31 -0300

    aprendi sobre if, elif, else e identação

--- aula016.py
+++ aula016.py
@@ -2,9 +2,17 @@
 
 entrada = input('Você deseja "entrar" ou "sair"? ')
 
-if entrada == 'entrar':
-    print('Você entrou no sistema')
-elif entrada == 'sair':
-    print('Você saiu do sistema')
-else:
-    print('inválido!')
+if entrada == 'entrar': # se entrada for igual a entrar
+    print('Você entrou no sistema') # ... 
+elif entrada == 'sair': # se não, se for igual a sair
+    print('Você saiu do sistema') # ... 
+else: # se não
+    print('inválido!') # ...
+
+# if = 'se'
+# elif = 'se não, se'
+# else = 'se não'
+
+'''
+O if pode existir sozinho, mas o elif e o else não, eles dependem de que um if exista anteriormente. Quando uma condição for verdadeira as condicionais são cessadas.
+'''
