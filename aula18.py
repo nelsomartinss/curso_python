2e6a4709bbfb68d5fbd59c2a70a4cd6a33d2a3df
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-09 11:54:44 -0300

    aprendi como usar o debuguer do vscode para entender como o programa roda

---
+++ aula18.py
@@ -0,0 +1,19 @@
+# Debuguer
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+
+if condicao1:
+    print('true')
+elif condicao2:
+    print('true')
+elif condicao3:
+    print('true')
+else:
+    print('true')
+
+if 10 == 10:
+    print(10)
+
+print('print fora do bloco')
