e13919abddc38e384a4c42522fe21d87efa05b4c
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-09 11:32:28 -0300

    aprendi sobre if, elif e else

---
+++ aula016.py
@@ -0,0 +1,10 @@
+# introdução ao if e else
+
+entrada = input('Você deseja "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('inválido!')
