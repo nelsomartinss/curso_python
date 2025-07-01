b01faa716cf7ab71b170797df450f1d621a241d7
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-01 18:53:47 -0300

    operadores de atribuição

---
+++ aula036.py
@@ -0,0 +1,31 @@
+'''
+Operadores de atribuição: =, +=, -=, *=, /=, //=, **=, %=
+'''
+
+# atribuindo
+contador = 1
+print(contador)
+
+# somando e atribuindo
+contador+=120
+print(contador)
+
+# multiplicando e atribuindo
+contador*=4
+print(contador)
+
+# dividindo e atribuindo
+contador/=2
+print(contador)
+
+# divisão inteira e atribuindo 
+contador//=3
+print(contador)
+
+# exponenciando e atribuindo 
+contador**=10
+print(contador)
+
+# resto da divisao e atribuindo
+contador%=2
+print(contador)
