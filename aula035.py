27698788829699ed8c14fb24ed9ad1ebec6c89b0
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-01 18:47:26 -0300

    while em detalhes - contadores

--- aula035.py
+++ aula035.py
@@ -1,1 +1,9 @@
-# While em detalhes
+# While em detalhes - contadores 
+
+contador = 0 # variavel de controle
+
+while contador <= 10: # enquanto o contador for menor ou igual a 10
+    print(contador, end=' ') # ele exibe o contador
+    contador+=1 # e soma mais um ao valor do contador
+    # quando o contador for igual a 10 ele para o loop
+print('Acabou!')
